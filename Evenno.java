162dbf7c01fb51501d2e2e71eb80223d44d50ea5
Giridharanathan-19 <giridharan2619@gmail.com>
2025-12-26 20:45:43 +0530

    Add files via upload

--- Evenno.java
+++ Evenno.java
@@ -3,10 +3,10 @@ public class Evenno {
     public static void main(String[] args) {
     Scanner sc=new Scanner(System.in);
     int num=sc.nextInt();
-    for(int i=0;i<=num;i++){
-        if(i%2==0)
+    for(int i=0;i<=num;i+=2){
+        
             System.out.println(i);
-        }
+    }
     sc.close();
     
         
