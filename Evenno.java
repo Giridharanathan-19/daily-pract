ddf8b428069020792053f9902401e5814347bafb
Giridharanathan-19 <giridharan2619@gmail.com>
2025-12-26 20:39:26 +0530

    Add files via upload

---
+++ Evenno.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Evenno {
+    public static void main(String[] args) {
+    Scanner sc=new Scanner(System.in);
+    int num=sc.nextInt();
+    for(int i=0;i<=num;i++){
+        if(i%2==0)
+            System.out.println(i);
+        }
+    sc.close();
+    
+        
+    }
+    
+}
