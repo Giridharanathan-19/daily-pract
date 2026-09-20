cd3248c931ab934fa80e6ce95398750abe52bb39
Giridharanathan-19 <giridharan2619@gmail.com>
2026-09-20 22:14:59 +0530

    initial commit

---
+++ Greatermultiple.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Greatermultiple{
+    public static void main(String[] args) {
+        Scanner obj = new Scanner(System.in);
+        int num=obj.nextInt();
+        int res=(num-(num%10))+10;
+        System.out.print(res);
+        
+        
+    }
+}
